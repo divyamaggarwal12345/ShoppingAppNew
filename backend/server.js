63190825f36cfe33353a16f0dbcd4c4d8a4b9f66
divv agg <aggdiv@gmail.com>
2022-01-22 14:53:35 +0530

    updated commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
-const connectDatabase = require("./config/database");
 const cloudinary = require("cloudinary");
+const connectDatabase = require("./config/database");
 
 // Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
@@ -9,16 +9,14 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
-//config
+// Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
-
-
-
 // Connecting to database
 connectDatabase();
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -29,8 +27,7 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
 
-
-  // Unhandled Promise Rejection
+// Unhandled Promise Rejection
 process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
   console.log(`Shutting down the server due to Unhandled Promise Rejection`);
